79ef8f4740679aa8ae7eea7388b79f8162395bdf
Md. Ariful Islam <gmarifulislamarif@gmail.com>
2019-07-16 00:01:59 +0600

    background image issue fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,10 +21,10 @@ set :repo_url, "git@github.com:arif362/eri.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+ append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -33,7 +33,7 @@ set :repo_url, "git@github.com:arif362/eri.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
